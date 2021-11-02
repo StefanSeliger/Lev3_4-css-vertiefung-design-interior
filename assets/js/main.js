d655f8582a969a43a9b719c1c895dbce315d2fc8
“StefanSeliger” <seligerstefan84@gmail.com>
2021-11-02 15:27:25 +0100

    neues projekt

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name")
+let mail = document.getElementById("mail")
+let message = document.getElementById("message")
+
+function submitData() {
+    console.log(name.value)
+    console.log(mail.value)
+    console.log(message.value)
+}
